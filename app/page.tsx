77ab7cfcace449316ddb65b2dadbdf3b93f42d9e
barrychung1112 <barrychung1112@gmail.com>
2025-04-05 17:05:04 -0700

    add tailwind css and animata.design package.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import BlurryBlob from "./components/animata/background/blurry-blob";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+  return <main className="h-screen flex items-center justify-center bg-gray-100">
+    <BlurryBlob
+      className="rounded-xl opacity-45"
+      firstBlobColor="bg-purple-400"
+      secondBlobColor="bg-blue-400"
+    />
+    <div className="animate-in fade-in zoom-in duration-1000 p-8 bg-white rounded-lg shadow-lg z-50">
+      🎉 Hello Tailwind Animate!
+    </div>
+  </main>
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const animate = require("tailwindcss-animate");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+          "pop-blob": {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+        colors: {
+          filter: {
+         "blur-20": "blur(20px)",
+         "blur-25": "blur(25px)",
+          },
+        },
+      animation: {
+        "pop-blob": "pop-blob 5s infinite",
+      }
+    },
+   },
+  },
+  plugins: [animate],
+};
