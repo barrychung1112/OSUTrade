f0d507041ef7aa170cd1bb4da251bffa3b1c5073
R4 Cheng <karaburi2023@gmail.com>
2025-04-05 20:06:28 -0700

    Implement product listing endpoint in API

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,16 @@
-import { type NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server';
+import ProductController from '@/../controller/productController';
+import ProductService from '../../../service/productService';
+import ProductRepository from '../../../repository/productRepository';
  
+const productController = new ProductController(new ProductService(new ProductRepository()));
+
 export async function GET(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers)
+  const products = await productController.listAllProducts();
+  return new Response(JSON.stringify(products), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
