413c6aa5860e18f75585ed5f74edcf5565a1cbb5
barrychung1112 <barrychung1112@gmail.com>
2026-09-17 19:02:54 -0700

    feat: add seasonal guides to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,7 @@ import type { MetadataRoute } from "next";
 import { SITE_URL } from "./lib/productMetadata";
 import { listPublicProducts } from "./lib/publicProduct";
 import { productPath, publicLocales } from "./lib/publicLocale";
+import { guidePaths } from "./lib/seasonalGuides";
 import { TRUST_PAGE_PATHS } from "./lib/trustPages";
 
 export const dynamic = "force-dynamic";
@@ -13,6 +14,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     { url: new URL("/", SITE_URL).toString() },
     { url: new URL("/overview", SITE_URL).toString() },
+    ...guidePaths.map((path) => ({ url: new URL(path, SITE_URL).toString() })),
     ...TRUST_PAGE_PATHS.map((path) => ({ url: new URL(path, SITE_URL).toString() })),
     ...products.flatMap((product) =>
       publicLocales.map((locale) => ({
